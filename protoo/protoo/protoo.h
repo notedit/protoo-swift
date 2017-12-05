149b6bfbf80e71ad5f8a038431296b9a8d9a7e71
notedit <notedit@gmail.com>
2017-12-05 20:07:02 +0800

    add code

---
+++ protoo/protoo/protoo.h
@@ -0,0 +1,19 @@
+//
+//  protoo.h
+//  protoo
+//
+//  Created by xiang on 05/12/2017.
+//  Copyright © 2017 dotEngine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for protoo.
+FOUNDATION_EXPORT double protooVersionNumber;
+
+//! Project version string for protoo.
+FOUNDATION_EXPORT const unsigned char protooVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <protoo/PublicHeader.h>
+
+
